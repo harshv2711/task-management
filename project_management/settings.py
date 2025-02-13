3b8ef989dab35a77d0337e7f56f941f8be177d2a
root <root@srv658856.hstgr.cloud>
2025-02-13 08:13:06 +0000

    Saving local changes before pull

--- project_management/settings.py
+++ project_management/settings.py
@@ -76,15 +76,17 @@ WSGI_APPLICATION = 'project_management.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_db',
+        'USER': 'postgres',
+        'PASSWORD': 'wapxj380',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
