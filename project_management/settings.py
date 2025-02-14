40cbb4e01ace0d86345d65ae9ac711b2320c8ffd
harsh <harsh@pivotmkg.com>
2025-02-14 17:48:39 +0000

    Untracked ignored files and updated .gitignore

--- project_management/settings.py
+++ project_management/settings.py
@@ -76,46 +76,14 @@ WSGI_APPLICATION = 'project_management.wsgi.application'
 
 
 # Database
-<<<<<<< HEAD
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases}
-=======
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'task_db',
-#         'USER': 'root',
-#         'PASSWORD': 'wapxj380',
-#         'HOST': 'localhost',  # Use 'localhost' if running locally
-#         'PORT': '3306',  # Default MySQL port
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },
-#     }
-# }
-
->>>>>>> ca13b150730d7a0870c6afe748544b8244bf8a52
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'task_db',
         'USER': 'postgres',
         'PASSWORD': 'wapxj380',
-<<<<<<< HEAD
         'HOST': 'localhost',  
         'PORT': '5432',
-=======
-        'HOST': 'localhost',  # Use the actual host, e.g., '127.0.0.1' or a remote server
-        'PORT': '5432',       # Default PostgreSQL port
->>>>>>> ca13b150730d7a0870c6afe748544b8244bf8a52
     }
 }
 
